ea81dd4f433e0923f5d1a9c15f45b2af9a4277f1
Mohammed81423 <mohammed81423nadeem@gmail.com>
2023-10-20 10:01:17 -0700

    initial commit

---
+++ day2.js/day2practiceset/script.js
@@ -0,0 +1,20 @@
+var str="learning javascript"
+console.log(str.toUpperCase())
+var str="hello javascript"
+console.log(str.toLowerCase())
+var str="hello wolrd"
+console.log(str.slice(0,5))
+var str="Facebook, Google, Microsoft, Apple, IBM, Oracle, amazon"
+console.log(str.split(","))
+var num="javascipt"
+console.log(num.indexOf("j"))
+console.log(num.lastIndexOf("t"))
+var str1="Javascript is used"
+var str2="to add functionalities to the web app"
+console.log(str1,str2.concat())
+var str="This course is all about javascript"
+console.log(str.includes("javascript"))
+console.log(Math.random()*100)
+console.log(Math.random()*50+50)
+console.log(11111,'\n',21248,'\n',313927,'\n',4141664,'\n',51525125)
+console.log(50000*12+10000)
---
+++ day2.js/script.js
@@ -0,0 +1,5 @@
+var num1=2
+var num2=3
+console.log(num1+num2)
+var str="hellowolrd"
+console.log(str.substr(4,6))
